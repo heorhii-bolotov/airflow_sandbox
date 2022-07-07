1335c381e4b8f4002c5b9d12edf700eb7b8acfe5
Heorhii <zhora2001zhora@gmail.com>
2022-07-07 22:31:09 +0300

    Added First Subdag

--- dags/parallel_dag.py
+++ dags/parallel_dag.py
@@ -1,10 +1,19 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
- 
+from airflow.operators.subdag_operator import SubDagOperator
+
+from subdags.subdag_parallel_dag import subdag_parallel_dag
+from utils.defaults import default_args
+
 from datetime import datetime
  
-with DAG('parallel_dag', start_date=datetime(2022, 1, 1), 
-    schedule_interval='@daily', catchup=False) as dag:
+with DAG(
+    'parallel_dag', 
+    start_date=datetime(2022, 1, 1), 
+    schedule_interval='@daily', 
+    catchup=False, 
+    default_args=default_args
+) as dag:
  
     extract_a = BashOperator(
         task_id='extract_a',
@@ -15,15 +24,17 @@ with DAG('parallel_dag', start_date=datetime(2022, 1, 1),
         task_id='extract_b',
         bash_command='sleep 1'
     )
- 
-    load_a = BashOperator(
-        task_id='load_a',
-        bash_command='sleep 1'
-    )
- 
-    load_b = BashOperator(
-        task_id='load_b',
-        bash_command='sleep 1'
+    
+    processing = SubDagOperator(
+        task_id='processing_tasks',
+        subdag=subdag_parallel_dag(
+            'parallel_dag',
+            'processing_tasks',
+            {
+                **default_args,
+                'start_date': datetime(2022, 1, 1)
+            }
+        )
     )
  
     transform = BashOperator(
@@ -31,6 +42,4 @@ with DAG('parallel_dag', start_date=datetime(2022, 1, 1),
         bash_command='sleep 1'
     )
  
-    extract_a >> load_a
-    extract_b >> load_b
-    [load_a, load_b] >> transform
+    [extract_a, extract_b] >> processing >> transform
---
+++ dags/subdags/subdag_parallel_dag.py
@@ -0,0 +1,21 @@
+from airflow import DAG
+from pydata_google_auth import default
+from airflow.operators.bash import BashOperator
+
+def subdag_parallel_dag(parent_dag_id, child_dag_id, default_args):
+    with DAG(
+        dag_id=f'{parent_dag_id}.{child_dag_id}',
+        default_args=default_args
+    ) as dag:
+        load_a = BashOperator(
+            task_id='load_a',
+            bash_command='sleep 1'
+        )
+        
+        load_b = BashOperator(
+            task_id='load_b',
+            bash_command='sleep 1'
+        )
+        
+        return dag
+        
